295b6b94ebd21c5f93ef0811056cf4f3987965bb
Brent Lippert <brentcl@uga.edu>
2016-11-17 13:26:10 -0500

    Renamed .scala object file to make more sense now that it contains not only the implementation for just a single tree

--- src/main/scala/GeneticProgram.scala
+++ src/main/scala/GeneticProgram.scala
@@ -4,7 +4,7 @@ import scala.util.Random
 /**
   * Created by Brent on 11/14/2016.
   */
-object Tree {
+object GeneticProgram {
 
   abstract class Tree
   case class Node(left: Tree, right: Tree, op: (Double, Double) => Double) extends Tree
