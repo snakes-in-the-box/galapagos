5c935b730ba87e8fcc571d0d286bc7181ced18b5
Brent Lippert <brentcl@uga.edu>
2016-11-29 00:18:30 -0500

    Added to the Pipeline the calculation of the paper's equation's wetbulb as well as adding this the each instances' mappping

--- src/main/scala/DataPipeline.scala
+++ src/main/scala/DataPipeline.scala
@@ -2,6 +2,8 @@
   * Created by Brent on 11/23/2016.
   */
 
+import breeze.numerics.{atan, pow}
+
 import scala.collection.mutable.HashMap
 import scala.io.Source
 import scala.util.Try
@@ -35,12 +37,20 @@ object DataPipeline {
     hashInstanceAux(split, 0, indexes)
   }
 
+  def calculateWetBulb(temp: Double, humidity: Double): Double = {
+    temp * atan(0.151977 * pow(humidity + 8.313659, 1/2)) + atan(temp + humidity) - atan(humidity - 1.676331) + 0.00391838 * pow(humidity, 3/2) * atan(0.023101 * humidity) - 4.686035
+  }
+
+  def addGroundTruth(inst: HashMap[String, Double]): HashMap[String, Double] = {
+    inst += ("wet-bulb" -> calculateWetBulb(inst("avg_temp_air_2m_C"), inst("avg_rh_2m_pct")))
+  }
+
   def readFile(fName: String): List[HashMap[String, Double]] = {
     val src = Source.fromFile(dirPath + fName)
     val fileList = src.getLines().toList
     val indexes = indexColumns(fileList.head)
     val instances = fileList.drop(1)
-    instances.map( s => hashInstance(s, indexes))
+    instances.map( s => addGroundTruth(hashInstance(s, indexes)))
   }
 
 }
