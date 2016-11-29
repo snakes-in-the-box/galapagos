88c2b71ea3894e3bc94088d9773dc58bef2c7d6b
Brent Lippert <brentcl@uga.edu>
2016-11-29 01:31:34 -0500

    Added methods in Driver for reading in and concatenating all data

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -2,14 +2,22 @@ import scala.collection.mutable.{HashMap, ListBuffer}
 import DataPipeline._
 import Tree._
 
+import scala.collection.mutable
 import scala.util.Random
 
 object Driver{
 
-  val dirPath = "C:/Users/Brent/Documents/School/DataPrac/FinalData/"
+  val dirPath = "C:/Users/Brent/Documents/School/DataPrac/FinalData/15_min/"
 
-  def importData(file: String): List[HashMap[String, Double]] = {
-    DataPipeline.readFile(dirPath + file)
+  def importFile(year: Int): List[HashMap[String, Double]] = {
+    DataPipeline.readFile(dirPath + year.toString + "-1.csv") ++: DataPipeline.readFile(dirPath + year.toString + "-2.csv")
+  }
+
+  def importData(start: Int, end: Int): List[HashMap[String, Double]] = {
+    if (start <= end) {
+      importFile(start) ++: importData(start+1, end)
+    }
+    else List[HashMap[String, Double]]()
   }
 
   def run(pop: ListBuffer[Tree], data: List[HashMap[Double, String]], tourSize: Int, maxGens: Int, ran: Random): Tree = {
